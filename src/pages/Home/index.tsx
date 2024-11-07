ea2f87780e8af95d2f93a4915d834ecf612d7570
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-07 14:17:21 -0300

    feat: :sparkles: add Hero items and change title color

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,12 @@
 import { LayoutContainer } from "../../layouts/DefaultLayout/styles";
-import { Background, Hero, TextContainer } from "./styles";
+import { Background, Hero, HeroItems, TextContainer } from "./styles";
 import heroCoffee from "../../assets/hero-coffee.svg";
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import { useTheme } from "styled-components";
 
 export function Home() {
+  const theme = useTheme();
+
   return (
     <Background>
       <LayoutContainer>
@@ -13,6 +17,44 @@ export function Home() {
               Com o Coffee Delivery você recebe seu café onde estiver, a
               qualquer hora
             </p>
+            <HeroItems>
+              <div>
+                <ShoppingCart
+                  size="2rem"
+                  weight="fill"
+                  color={theme["base-100"]}
+                  style={{ backgroundColor: theme["primary-800"] }}
+                />
+                <span>Compra simples e segura</span>
+              </div>
+              <div>
+                <Package
+                  size="2rem"
+                  weight="fill"
+                  color={theme["base-100"]}
+                  style={{ backgroundColor: theme["base-700"] }}
+                />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+              <div>
+                <Timer
+                  size="2rem"
+                  weight="fill"
+                  color={theme["base-100"]}
+                  style={{ backgroundColor: theme["primary-600"] }}
+                />
+                <span>Entrega rápida e rastreada</span>
+              </div>
+              <div>
+                <Coffee
+                  size="2rem"
+                  weight="fill"
+                  color={theme["base-100"]}
+                  style={{ backgroundColor: theme["secondary-500"] }}
+                />
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </HeroItems>
           </TextContainer>
           <img src={heroCoffee} alt="coffee cup" />
         </Hero>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import heroBackground from "../../assets/hero-background.png";
-import { TITLE_XL, TEXT_L } from "../../styles/variables";
+import { TITLE_XL, TEXT_L, TEXT_M } from "../../styles/variables";
 
 export const Background = styled.div`
-  background: url(${heroBackground}) center / cover no-repeat;
+  /* background: url(${heroBackground}) center / 90rem 34rem no-repeat; */
+  background: url(${heroBackground}) center / auto auto no-repeat;
   /* background-image: url(${heroBackground});
   background-position: center center;
   background-size: contain;
@@ -17,7 +18,7 @@ export const Hero = styled.div`
   align-items: center;
 
   img {
-    height: 22.5rem;
+    width: 29.75rem;
   }
 `;
 
@@ -26,9 +27,34 @@ export const TextContainer = styled.div`
   h1 {
     font: ${TITLE_XL};
     padding-bottom: 1rem;
+    color: ${(props) => props.theme["base-900"]};
   }
   p {
     font: ${TEXT_L};
     padding-bottom: 4.125rem;
+    color: ${(props) => props.theme["base-800"]};
+  }
+`;
+
+export const HeroItems = styled.div`
+  display: grid;
+  grid-template-columns: 0.9fr 1.1fr;
+  grid-auto-rows: 1fr;
+  grid-column-gap: 1.875rem;
+  grid-row-gap: 1.25rem;
+  max-width: 35.4375rem;
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    span {
+      font: ${TEXT_M};
+      color: ${(props) => props.theme["base-700"]};
+    }
+    svg {
+      min-width: 2rem;
+      padding: 0.5rem;
+      border-radius: 50%;
+    }
   }
 `;
